706919d6ea7dcb04b7221ca2fde8fed30f61f8d0
Karl Orjalo <karlorjalo@gmail.com>
2025-07-29 16:47:13 +0800

    feat: Add routes dir and server skeleton

--- backend/controllers/user.go
+++ backend/controllers/user.go
@@ -1,1 +1,30 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"server/config"
+	"server/services"
+
+	"github.com/patrickmn/go-cache"
+)
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var output interface{}
+	value, found := config.Cache.Get("Users")
+	if found {
+		output = value
+	} else {
+		arr, err := services.GetUsers()
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(500)
+		}
+		output = arr
+		config.Cache.Set("Users", arr, cache.DefaultExpiration)
+	}
+	w.WriteHeader(200)
+	formatted, _ := json.Marshal(output)
+	w.Write(formatted)
+}
